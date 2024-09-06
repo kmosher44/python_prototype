80ebd9caaa9a1ea4752e9ca73b89745d20883a26
keith <keith@extensibleenergy.com>
2024-09-06 11:24:58 -0700

    more string manipulation

--- basic/demonstrate_string_manipulation.py
+++ basic/demonstrate_string_manipulation.py
@@ -4,13 +4,14 @@ from demonstrate_data_types import demonstrate_data_types
 # Python Function use and behavior
 def demonstrate_string_manipulation() -> None:
     demonstrate_strip_and_index(" string-a-ling-ling ")
+    demonstrate_split_and_replace(" lorem ipsum dolor ", "ipsum")
 
 def demonstrate_strip_and_index(str_to_print: str) -> None:
     # Trim strings with .strip
     # Access chars in string using index
 
     print(f"{len(str_to_print)}: ###{str_to_print}###")
-    trimmed_string = str_to_print.strip()
+    trimmed_string: str = str_to_print.strip()
     print(f"{len(trimmed_string)}: ###{trimmed_string}###")
 
     # Forwards
@@ -19,3 +20,16 @@ def demonstrate_strip_and_index(str_to_print: str) -> None:
     # Backwards
     for i in range((len(str_to_print) - 1), 0, -1):
         print(str_to_print[i])
+
+def demonstrate_split_and_replace(str_to_print: str, str_to_replace: str) -> None:
+    # Split the string
+    # Replace parts of the string
+
+    print(f"{len(str_to_print)}: ###{str_to_print}###")
+    word_list: list[str] = str_to_print.strip().split(" ")
+    print(f"word list: length = {len(word_list)}, {word_list}")
+    for word in word_list:
+        print(word)
+
+    replaced_str: str = str_to_print.replace(str_to_replace, "heartlessly replaced")
+    print(f"{len(replaced_str)}: ###{replaced_str}###")
